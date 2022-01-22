6c3771b6ed86b7be9f543d6bff9e700af2d95ec1
Trey Hope <trey.a.hope@gmail.com>
2022-01-22 11:06:20 -0500

    * Now using Algolia Firebase extension, so I removed my Algolia Google Cloud Function.

--- web/functions/index.js
+++ web/functions/index.js
@@ -1,8 +1,4 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const Algolia = require('./algolia/index'); 
 
 admin.initializeApp(functions.config().firebase);
-
-//Algolia
-exports.AlgoliaSyncUsers = Algolia.algoliaSyncUsers;
