70eadec6660a514c0d01391f4f5af24b61944250
Trey Hope <trey.a.hope@gmail.com>
2022-01-25 01:31:02 -0500

    * Now sending notification with scheduler.

--- web/functions/scheduler/scheduler.js
+++ web/functions/scheduler/scheduler.js
@@ -1,15 +1,15 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-//“At 00:00 on day-of-month 1.”
-exports.addCoinsToAllUsers = functions.pubsub.schedule('21 23 * * *').timeZone('America/New_York').onRun(async (context) => {
+//“At 5pm every Friday”
+exports.addCoinsToAllUsers = functions.pubsub.schedule('00 17 * * 5').timeZone('America/New_York').onRun(async (context) => {
     //https://crontab.guru/
 
     //Query of all users in database.
     const usersSnapshot = await admin.firestore().collection('users').get();
 
     //Coins that will be added to each user.
-    const coins = 10;
+    const coins = 50;
 
     //Iterate over each user in the database.
     await Promise.all(usersSnapshot.docs.map(
@@ -17,27 +17,21 @@ exports.addCoinsToAllUsers = functions.pubsub.schedule('21 23 * * *').timeZone('
             //Extract data from user document.
             const data = userDoc.data();
 
-            //Add coins to this user.
-            if (data['uid'] === 'LZHPLuXGJzNxXnv97xgcJUApuIz2') {
-                userDoc.ref.update({ 'coins': admin.firestore.FieldValue.increment(coins) });
+            //Add coins to user.
+            userDoc.ref.update({ 'coins': admin.firestore.FieldValue.increment(coins) });
 
-                // //Send notification to user.
-                // if (data['fcmToken'] !== null) {
-                //     //Create payload.
-                //     const payload = {
-                //         token: data['fcmToken'],
-                //         notification: {
-                //             title: 'HAPPY FRIDAY!',
-                //             body: `You just received ${coins} free coins!`,
-                //         },
-                //         data: {
-                //             body: message,
-                //         }
-                //     };
+            //Send notification to user.
+            if (data['fcmToken'] !== null) {
+                //Create payload. 
+                var message = {
+                    notification: {
+                        title: 'HAPPY FRIDAY!',
+                        body: `You just received ${coins} free coins!`,
+                    },
+                };
 
-                //     //Send message.
-                //     admin.messaging().send(payload);
-                // }
+                //Send message.
+                admin.messaging().sendToDevice(data['fcmToken'], message);
             }
         }
     ))
